38b68c4464cc306da7181f34f8ea2f3c3a109b4b
shararehl <sharareh.lotfi@gmail.com>
2017-08-17 17:20:14 +0430

    Tailors name added

--- src/mocks/providers/items.ts
+++ src/mocks/providers/items.ts
@@ -8,48 +8,48 @@ export class Items {
   items: Item[] = [];
 
   defaultItem: any = {
-    "name": "Burt Bear",
+    "name": "شهرزاد دوستی ",
     "profilePic": "assets/img/speakers/bear.jpg",
-    "about": "Burt is a Bear.",
+    "about": "طراحی و دوخت انواع لباس های عروس و شب",
   };
 
 
   constructor(public http: Http) {
     let items = [
       {
-        "name": "Burt Bear",
+        "name": "شهرزاد دوستی ",
         "profilePic": "assets/img/speakers/bear.jpg",
-        "about": "Burt is a Bear."
+        "about": "طراحی و دوخت انواع لباس های عروس و شب"
       },
       {
-        "name": "Charlie Cheetah",
+        "name": "هنگامه سلیمی",
         "profilePic": "assets/img/speakers/cheetah.jpg",
-        "about": "Charlie is a Cheetah."
+        "about": "طراحی و دوخت لباس های بچه گانه"
       },
       {
-        "name": "Donald Duck",
+        "name": "یاس فرابی",
         "profilePic": "assets/img/speakers/duck.jpg",
-        "about": "Donald is a Duck."
+        "about": "طراحی و دوخت لباس مردانه رسمی و اسپورت"
       },
       {
-        "name": "Eva Eagle",
+        "name": "گلنار توانا",
         "profilePic": "assets/img/speakers/eagle.jpg",
         "about": "Eva is an Eagle."
       },
       {
-        "name": "Ellie Elephant",
+        "name": "کلانتر هرمزی",
         "profilePic": "assets/img/speakers/elephant.jpg",
-        "about": "Ellie is an Elephant."
+        "about": "طراحی و دوخت کت و شلوار"
       },
       {
-        "name": "Molly Mouse",
+        "name": "گلشن میناوند",
         "profilePic": "assets/img/speakers/mouse.jpg",
-        "about": "Molly is a Mouse."
+        "about": "طراحی و دوخت لباس عروس"
       },
       {
-        "name": "Paul Puppy",
+        "name": "گل رز",
         "profilePic": "assets/img/speakers/puppy.jpg",
-        "about": "Paul is a Puppy."
+        "about": "طراحی و دوخت لباس مجلسی"
       }
     ];
 
