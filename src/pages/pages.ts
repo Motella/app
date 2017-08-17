a14b8cbcb343f1c109e37c60788ee63542c8de27
alirezavafi <ar.vafi@gmail.com>
2017-08-17 17:44:06 +0430

    mk

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -12,6 +12,6 @@ export const FirstRunPage = TutorialPage;
 export const MainPage = TabsPage;
 
 // The initial root pages for our tabs (remove if not using tabs)
-export const Tab1Root = ListMasterPage;
+export const Tab1Root = SettingsPage;
 export const Tab2Root = SearchPage;
-export const Tab3Root = SettingsPage;
+export const Tab3Root = ListMasterPage;
